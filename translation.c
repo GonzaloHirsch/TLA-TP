77fb79390eddc79abcdc447d615b4fc583b9543c
francisco choi <fchoi@itba.edu.ar>
2020-05-31 23:10:52 -0300

    added gen exp

--- translation.c
+++ translation.c
@@ -24,6 +24,7 @@ char * process(GenericNode * gn){
             value = processEntrypointNode(gn);
             break;
         case NODE_IFSENTENCE:
+            value = processIf(gn);
             break;
         case NODE_ELSETRAIN:
             break;
@@ -60,6 +61,10 @@ char * process(GenericNode * gn){
             break;
         case NODE_WHILE:
             value = processWhileNode(gn);
+            break;
+
+        case NODE_G_EXPRESSION:
+            value = processGeneralExpression(gn);
             break;
 
 
@@ -209,6 +214,7 @@ char * processStamentListNode(GenericNode * gn){
 
     return buffer;
 }
+
 
 
 char * processNodeList(NodeList * current){
@@ -413,7 +419,7 @@ char * processBlock(GenericNode * gn){
 
     buffer = realloc(buffer, strlen(op_b) + strlen(processedBlock) + strlen(cl_b) + strlen(buffer));
     
-    if(buffer == NULL | child == NULL){
+    if(buffer == NULL){
         free(processedBlock);
         free(buffer);
         return NULL;
@@ -429,6 +435,71 @@ char * processBlock(GenericNode * gn){
 
     return buffer;
 
+}
+
+
+
+char * processGeneralExpression(GenericNode * gn){
+    char * buffer = malloc(1);
+    if (buffer == NULL) {
+        return NULL;
+    }
+    buffer[0] = '\0';
+    char * operatorValue = gn -> value;
+    char * op;
+
+    if(strcmp(operatorValue, "AND") == 0){
+        op = " && ";
+    }
+    else if(strcmp(operatorValue, "OR") == 0)
+    {   
+        op = " | ";
+    }
+    else{
+        op = "";
+    }
+
+    GenericNode * exp1 = gn -> children ->current;
+    char * exp1Proc = process(exp1);
+
+
+    if(strcmp(op, "") != 0){ //there is an operator to parse, then there is another expression
+        GenericNode * exp2 = gn -> children -> next ->current;
+        char * exp2Proc = process(exp2);
+        if(exp2Proc == NULL){
+            free(buffer);
+            return NULL;
+        }
+        buffer = realloc(buffer, strlen(exp1Proc) + strlen(op) + strlen(exp2Proc) + strlen(buffer));
+        if(buffer == NULL){
+            free(exp1Proc);
+            free(exp2Proc);
+            return NULL;
+        }
+        strcat(buffer, exp1Proc);
+        strcat(buffer, op);
+        strcat(buffer, exp2Proc);
+
+        free(exp1Proc);
+        free(exp2Proc);
+
+        return buffer;
+
+    }
+
+    // there is no other expression
+
+    buffer = realloc(buffer, strlen(exp1Proc) + strlen(buffer));
+    if(buffer == NULL){
+        free(exp1Proc);
+        return NULL;
+    }
+
+    strcat(buffer, exp1Proc);
+
+    free(exp1Proc);
+
+    return buffer;
 }
 
 // -------------------------- EXPOSED FUNCTIONS --------------------------
