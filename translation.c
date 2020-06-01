ddc5a76cdf10649910c3647202fd6eb2e61b1256
igrib <igrib@example.com>
2020-05-31 23:41:46 -0300

    [Fix] Removed node deletion in translates

--- translation.c
+++ translation.c
@@ -131,8 +131,6 @@ char * processEntrypointNode(GenericNode * gn){
     char * buffer = malloc(bufferSize);
     if (buffer == NULL){
         free(statements);
-        free(statementsListNode);
-        free(statementsNode);
         return NULL;
     }
 
@@ -181,10 +179,6 @@ char * processWhileNode(GenericNode * gn){
     if (buffer == NULL){
         free(block);
         free(expression);
-        free(expressionListNode);
-        free(expressionNode);
-        free(blockListNode);
-        free(blockNode);
         return NULL;
     }
 
@@ -334,7 +328,6 @@ char * processAssignment(GenericNode * gn){
     strcat(buffer, valueNListProc);
 
     free(varName);
-    free(valueNList);
 
     return buffer;
 }
