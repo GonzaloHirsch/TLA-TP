ebb05183b9482e8f69717a6e68fdc395ebe0a7e0
igrib <igrib@example.com>
2020-06-01 01:11:18 -0300

    [DeclTranslation] vardeclassignment

--- translation/declTranslation.c
+++ translation/declTranslation.c
@@ -7,15 +7,28 @@ char * processVarDeclaration(GenericNode * gn) {
     nl = nl->next;
     char * var = translate(nl->current);
     char * numberLiteral = NULL;
+    char * buffer;
     if (nl->next != NULL) {
         nl = nl->next;
         numberLiteral = translate(nl->current);
+        buffer = malloc(strlen(type) + strlen(var) + strlen(numberLiteral) + 2); // 2 for brackets
+        sprintf(buffer, "%s %s[%s]\n", type, var, numberLiteral);
     }
-    char * buffer = malloc(strlen(type) + strlen(var) + (numberLiteral == NULL) ? 0 : strlen(numberLiteral));
-    sprintf(buffer, "%s %s = %s\n", type, var, numberLiteral);
+    else {
+        buffer = malloc(strlen(type) + strlen(var));
+        sprintf(buffer, "%s %s[%s]\n", type, var, numberLiteral);
+    }
     return buffer;
 }
 char * processVarDeclassignment(GenericNode * gn) {
-    
-    return "processVarDeclassignment";
+    NodeList * nl = gn->children;
+    //TODO check nullity
+    char * type = translate(nl->current);
+    nl = nl->next;
+    char * var = translate(nl->current);
+    nl = nl->next;
+    char * value = translate(nl->current);
+    char * buffer = malloc(strlen(type) + strlen(var) + strlen(value));
+    sprintf(buffer, "%s %s = %s", type, var, value);
+    return buffer;
 }
