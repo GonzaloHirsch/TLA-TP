e9866d664350f4357434d63dd487f00ee2736894
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-06-05 19:49:57 -0300

    [Fix] Fix in node to run foreach

--- node.c
+++ node.c
@@ -82,7 +82,7 @@ void printGenericNode(GenericNode * gn, int tabs) {
 }
 
 void changeDescendantVarType(GenericNode * gn, char * varName, VarType targetVarType, int isMeta) {
-    if (gn->info.type == NODE_VARIABLE && strcmp(gn->value, varName) == 0){
+    if (gn->info.type == NODE_VARIABLE_REF && strcmp(gn->value, varName) == 0){
         gn->info.varType = targetVarType;
         if (isMeta)
             gn->info.isMeta = isMeta;
